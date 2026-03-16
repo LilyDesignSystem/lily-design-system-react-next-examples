09a5851665a9905ffbc24bfced869f03268e6b24
Joel Parker Henderson <joel@joelparkerhenderson.com>
2026-03-16 23:37:46 +0000

    Add espana-tarjeta-sanitaria-individual-*

---
+++ components/EspanaTarjetaSanitariaIndividualInput.tsx
@@ -0,0 +1,2 @@
+export { default } from "@pgds/EspanaTarjetaSanitariaIndividualInput";
+export type * from "@pgds/EspanaTarjetaSanitariaIndividualInput";
---
+++ components/EspanaTarjetaSanitariaIndividualView.tsx
@@ -0,0 +1,2 @@
+export { default } from "@pgds/EspanaTarjetaSanitariaIndividualView";
+export type * from "@pgds/EspanaTarjetaSanitariaIndividualView";
